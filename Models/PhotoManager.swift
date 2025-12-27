def3350827fb7dc71f594037e32b50c10c6c1d47
Your Name <your-email@example.com>
2025-12-27 20:06:33 +0400

    Add Combine import for ObservableObject protocol

--- Models/PhotoManager.swift
+++ Models/PhotoManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Photos
 import UIKit
+import Combine
 
 class PhotoManager: ObservableObject {
     @Published var authorizationStatus: PHAuthorizationStatus = .notDetermined
